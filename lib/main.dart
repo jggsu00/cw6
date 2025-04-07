d272846e0da3b39dbd8f2773bf09f4ab47f385c0
John Gomez <johngomez45@Johns-MacBook-Pro.local>
2025-04-07 17:49:47 -0400

    created task model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'login_screen.dart';
+import 'task_list_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
